5b019932eef83cdac68349e174a02dca067da3cc
Stephan Martin <designer2k2@gmail.com>
2019-11-01 11:07:01 +0100

    added logging function
    logging added to help diagnosing problems

--- warpigui.py
+++ warpigui.py
@@ -12,10 +12,20 @@
 # kismet conf must be correct! 
 # gpsd will be called, check that it works with UART
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+					filename='warpi.log')
+
+logging.info('Startup')
+
 #fake cpuinfo:
 from subprocess import call
 call("sudo mount -v --bind /root/fake-cpuinfo /proc/cpuinfo", shell=True)
 call("hwclock -s", shell=True)
+
+logging.debug('HW Clock synced')
 
 import board
 import busio
@@ -34,6 +44,12 @@ import RPi.GPIO as GPIO
 #the konverter tool:
 import kismettowigle
 
+logging.debug('All imports done')
+
+#Turn some logger to only show warnings:
+logging.getLogger("gpsd").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
 
@@ -46,6 +62,8 @@ disp.rotation = 2
 
 # Input Pin:
 GPIO.setmode(GPIO.BCM) 
+
+logging.debug('IO Setup')
 
 # Interrupts:
 Counter = 0
@@ -87,6 +105,9 @@ GPIO.add_event_detect(17, GPIO.RISING, callback=InterruptDown, bouncetime=300)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 GPIO.add_event_detect(23, GPIO.RISING, callback=InterruptLeft, bouncetime=300)
 
+
+logging.debug('GPIO Setup done')
+
 # Clear display.
 disp.fill(0)
 disp.show()
@@ -107,6 +128,8 @@ draw.rectangle((0, 0, width, height), outline=0, fill=0)
 font = ImageFont.truetype('/home/pi/Minecraftia.ttf', 8)
 fontbig = ImageFont.truetype('/home/pi/arial.ttf', 24)
 
+logging.debug('Display setup done')
+
 # set country code
 # call("iw reg set AT", shell=True)
 
@@ -119,7 +142,7 @@ autostart = 10
 autostarted = False
 
 def startservice():
-    print("Starting GPSD / Kismet")
+    logging.info("Starting GPSD / Kismet")
     call("gpsd /dev/serial0", shell=True)
     call("sleep 1", shell=True)
     call('screen -dm bash -c "kismet"', shell=True)
@@ -127,13 +150,16 @@ def startservice():
     gpsrun = True
 	
 def stopservice():
-    print("Stopping GPSD / Kismet")
+    logging.info("Stopping GPSD / Kismet")
     global gpsrun
     gpsrun = False
     call("killall gpsd", shell=True)
     call("killall kismet", shell=True)
 	
 def freboot():
+    logging.info("Rebooting")
+    global looping
+    looping = False
     disp.fill(0)
     disp.show()
     call("sleep 1", shell=True)
@@ -141,20 +167,32 @@ def freboot():
     quit()
 
 def fshutdown():
+    global looping
+    looping = False
+    logging.info("Shutdown")
     call("killall kismet", shell=True)
-    call("sleep 1", shell=True)
-    convertall()
-    call("sleep 1", shell=True)
-    disp.fill(0)
+    #call("sleep 1", shell=True)
+    logging.debug("Kismet shutdown")
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((0, 20),'Convert',font=fontbig, fill=255)	
+    disp.image(image)	
     disp.show()
+    convertall()
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((0, 20),'Shutdown',font=fontbig, fill=255)	
+    disp.image(image)	
+    disp.show()
+    logging.debug("LCD Black")
+    call("sleep 1", shell=True)
     call("sudo shutdown -h now", shell=True)
+    logging.debug("shutdown -h triggered")
     quit()
 		
 def list_files1(directory, extension):
     return (f for f in listdir(directory) if f.endswith('.' + extension))
 	
 def convertall():
-    print("Convert kismets")
+    logging.debug("Convert kismets")
     #only do this when the kismet it not running
     if not gpsrun:
         list = list_files1('/','kismet')
@@ -164,13 +202,15 @@ def convertall():
             csvfilename = (''.join(them.split('.')[:-1])) + ('.CSV')
             #print(csvfilename)
             if not os.path.exists('/'+csvfilename):
-                print("CSV not found, create it "+str(csvfilename))
+                logging.debug("CSV not found, create it "+str(csvfilename))
                 kismettowigle.main('/'+them)
-    print("Convert from all done")
+    logging.debug("Convert from all done")
 
-print("All setup, go into loop")
+logging.debug('All setup, go into loop')
 
-while True:
+looping = True
+
+while looping:
 
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 	
@@ -193,14 +233,22 @@ while True:
         try:
             gpsd.connect()
             packet = gpsd.get_current()		
-            draw.text((0, 10),'GPS:  ' + str(packet.mode) + '  SAT:  ' + str(packet.sats) + '  USED:  ' + str(packet.sats_valid),font=font, fill=255)
+            draw.text((0, 10),'GPS:  ' + str(packet.mode) + '  SAT:  ' + str(packet.sats) + '  Use:  ' + str(packet.sats_valid),font=font, fill=255)
+            if packet.mode == 0:
+                draw.rectangle((115, 20, width-2, 10), outline=0, fill=0)
+            if packet.mode == 1:
+                draw.rectangle((120, 18, width-4, 14), outline=255, fill=0)
+            if packet.mode == 2:
+                draw.rectangle((120, 18, width-4, 14), outline=255, fill=1)			
+            if packet.mode == 3:
+                draw.rectangle((115, 20, width-2, 10), outline=255, fill=1)		
             conn = kismet_rest.KismetConnector(username='root',password='toor')
             devices = conn.system_status()['kismet.system.devices.count']
             kismetmemory = conn.system_status()['kismet.system.memory.rss']
             draw.text((0, 20),'D {:>7}'.format(devices),font=fontbig, fill=255)
             draw.text((0, 44),'Kismet mem: {:>4.0f}mb'.format(kismetmemory/1000),font=font, fill=255)
-        except:
-            print("An exception occurred " + str(gpsrun)) 
+        except Exception as e:
+            logging.error("An exception occurred " + str(e)) 
 	
     if not autostarted:	
         if autostart > 0:
@@ -216,3 +264,7 @@ while True:
 
 	#wait a bit:
     sleep(sleeptime)
+
+
+while True:
+    sleep(10)
