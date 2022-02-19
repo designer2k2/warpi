4bb7581c7f71a1abf7542b249097e74c5a5131cb
Stephan Martin <designer2k2@gmail.com>
2022-02-19 10:12:53 +0100

    initial install script
    bash script to install some of the programs

---
+++ warpi_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# setup some needed parts for the warpi
+echo "Start warpi needed program install"
+apt update
+apt -y install kismet
+apt -y install python3-smbus
+apt -y install i2c-tools
+apt -y install gpsd gpsd-clients
+apt -y install realtek-rtl88xxau-dkms
+apt -y install ntfs-3g
+apt -y install exfat-fuse
+apt -y install python3-pip
+echo "remove large not needed programs"
+apt -y remove metasploit-framework firefox-esr exploitdb powershell-empire
+apt -y autoremove
+echo "upgrade all to have the latest and greatest"
+apt -y upgrade
+echo "Download warpi script"
+wget https://github.com/designer2k2/warpi/raw/master/warpigui.py
+wget https://github.com/designer2k2/warpi/raw/master/kismettowigle.py
+wget https://github.com/designer2k2/warpi/raw/master/requirements.txt
+echo "Install warpi python requirements"
+pip3 install -r requirements.txt
+echo "finished"
