5ae67fab3350fed2d0b36b2ce15bbb57f23ac94b
Stephan Martin <designer2k2@gmail.com>
2023-06-12 19:00:06 +0200

    black code formatting

--- warpigui.py
+++ warpigui.py
@@ -304,10 +304,10 @@ while looping:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.settimeout(0)
         try:
-            s.connect(('10.254.254.254', 1))
+            s.connect(("10.254.254.254", 1))
             rpiIP = s.getsockname()[0]
         except Exception:
-            rpiIP = '127.0.0.1'
+            rpiIP = "127.0.0.1"
         finally:
             s.close()
         draw.text(
