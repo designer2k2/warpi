05ced02f847534acfe46a6664e8a71be4ec3855b
Stephan Martin <designer2k2@gmail.com>
2025-05-22 18:47:26 +0200

    Update setup script with venv creation
    move script into python venv

--- warpi_setup.sh
+++ warpi_setup.sh
@@ -6,7 +6,6 @@ apt -y install kismet
 apt -y install python3-smbus
 apt -y install i2c-tools
 apt -y install gpsd gpsd-clients
-apt -y install realtek-rtl88xxau-dkms
 apt -y install ntfs-3g
 apt -y install exfat-fuse
 apt -y install python3-pip
@@ -19,6 +18,22 @@ echo "Download warpi script"
 wget https://github.com/designer2k2/warpi/raw/master/warpigui.py
 wget https://github.com/designer2k2/warpi/raw/master/requirements.txt
 wget https://github.com/designer2k2/warpi/raw/master/PixeloidSans.ttf
-echo "Install warpi python requirements"
-pip3 install -r requirements.txt
+echo "Install warpi python in venv with requirements"
+VENV_DIR="venv"
+REQUIREMENTS_FILE="requirements.txt"
+# Create venv if it doesn't exist
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating virtual environment '$VENV_DIR'..."
+    python3 -m venv –system-site-packages "$VENV_DIR" || { echo "Failed to create virtual environment."; exit 1; }
+    echo "Virtual environment '$VENV_DIR' created."
+fi
+
+# Install requirements
+if [ -f "$REQUIREMENTS_FILE" ] && [ -s "$REQUIREMENTS_FILE" ]; then
+    echo "Installing packages from '$REQUIREMENTS_FILE'..."
+    "$VENV_DIR/bin/pip" install -U -r "$REQUIREMENTS_FILE" || echo "Failed to install requirements from '$REQUIREMENTS_FILE'."
+else
+    echo "No '$REQUIREMENTS_FILE' found or it is empty. Skipping package installation."
+fi
 echo "finished"
+# apt -y install realtek-rtl88xxau-dkms
